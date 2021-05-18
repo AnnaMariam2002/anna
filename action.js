fe11ac7c47491995de1b25b467ef2a0a68310496
AnnaMariam2002 <84407410+AnnaMariam2002@users.noreply.github.com>
2021-05-18 23:33:06 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,3 @@
+function pinkbutton(){document.body.style.background="pink"}
+function greenbutton(){document.body.style.background="green"}
+function yellowbutton(){document.body.style.background="yellow"}
